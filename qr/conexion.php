3bed0b81f83acdab4f6512ab447d6307232e6f74
rider77 <rooqter@hotmail.com>
2014-01-26 19:40:50 -0600

    Archivo de conexion

--- qr/conexion.php
+++ qr/conexion.php
@@ -2,3 +2,5 @@
 $link = mysql_connect("localhost","root","");
 mysql_select_db("lag",$link);
 
+// Este archivo no hay que manejarlo en github.
+// Solo el beta, cuando este ya en el servidor no lo ponemos aqui.
